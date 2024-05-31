862613ba9682a428c37e1cce5196b8c236290365
Tolux001 <toluwalasepeter100@gmail.com>
2024-05-31 16:29:43 +0100

    Scripting pause && play

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // MDN
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and sub frames to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+const btn = document.querySelector(".switch-btn");
+const videoContainer = document.querySelector(".video-container");
+const switchButton = document.querySelector(".switch");
+
+btn.addEventListener("click", () => {
+	if (!videoContainer.paused) {
+		videoContainer.pause();
+		switchButton.classList.add("slide");
+	} else {
+		videoContainer.play();
+		switchButton.classList.remove("slide");
+	}
+});
